25c9734816c0bd3108cb5212d632128c2441ac3e
HJoo <yoon3798@naver.com>
2023-05-25 13:45:10 +0900

    chore : 패키지 구조 설정

---
+++ src/main/java/com/phoenix/assetbe/AssetBeApplication.java
@@ -0,0 +1,13 @@
+package com.phoenix.assetbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssetBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AssetBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/phoenix/assetbe/AssetBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phoenix.assetbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssetBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
