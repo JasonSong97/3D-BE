085da847c34766bdde2b6be56b07439f98971bf4
HJoo <yoon3798@naver.com>
2023-06-01 19:11:06 +0900

    chore : #1 더미 객체 설정

---
+++ src/main/java/com/phoenix/assetbe/core/dummy/DataInit.java
@@ -0,0 +1,19 @@
+package com.phoenix.assetbe.core.dummy;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataInit extends DummyEntity{
+
+    @Profile("dev")
+    @Bean
+    CommandLineRunner init(UserRepository userRepository){
+        return args -> {
+            userRepository.save(newUser("ssar", "쌀"));
+            userRepository.save(newUser("cos", "코스"));
+        };
+    }
+}
---
+++ src/main/java/com/phoenix/assetbe/core/dummy/DummyEntity.java
@@ -0,0 +1,34 @@
+package com.phoenix.assetbe.core.dummy;
+
+import com.phoenix.assetbe.model.user.Role;
+import com.phoenix.assetbe.model.user.Status;
+import com.phoenix.assetbe.model.user.User;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.time.LocalDateTime;
+
+public class DummyEntity {
+    public User newUser(String firstname, String lastname){
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        return User.builder()
+                .firstname(firstname)
+                .lastname(lastname)
+                .password(passwordEncoder.encode("1234"))
+                .email(firstname + lastname +"@nate.com")
+                .role(Role.USER)
+                .status(Status.ACTIVE)
+                .build();
+    }
+
+    public User newMockUser(String firstname, String lastname){
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        return User.builder()
+                .firstname(firstname)
+                .lastname(lastname)
+                .password(passwordEncoder.encode("1234"))
+                .email(firstname + lastname +"@nate.com")
+                .role(Role.USER)
+                .status(Status.ACTIVE)
+                .build();
+    }
+}
