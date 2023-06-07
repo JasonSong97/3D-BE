65b978138ae5eafa90403820505873fd9a75adc2
BE_YangJinho <real.great.code@gmail.com>
2023-06-07 10:25:28 +0900

    feat : #35 CategoryRepositoryImpl countByCategory & countBySubCategory

--- src/main/java/com/phoenix/assetbe/model/asset/CategoryRepositoryCustom.java
+++ src/main/java/com/phoenix/assetbe/model/asset/CategoryRepositoryCustom.java
@@ -1,4 +1,11 @@
 package com.phoenix.assetbe.model.asset;
 
+import com.phoenix.assetbe.dto.asset.AssetResponse;
+
+import java.util.List;
+
 public interface CategoryRepositoryCustom {
+    List<AssetResponse.CountOutDTO.CountCategory> countByCategory();
+
+    List<AssetResponse.CountOutDTO.CountSubCategory> countBySubCategory();
 }
--- src/main/java/com/phoenix/assetbe/model/asset/CategoryRepositoryImpl.java
+++ src/main/java/com/phoenix/assetbe/model/asset/CategoryRepositoryImpl.java
@@ -1,4 +1,34 @@
 package com.phoenix.assetbe.model.asset;
 
-public class CategoryRepositoryImpl implements CategoryRepositoryCustom{
+import com.phoenix.assetbe.dto.asset.AssetResponse;
+import com.querydsl.core.types.Projections;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+public class CategoryRepositoryImpl implements CategoryRepositoryCustom {
+
+    private final JPAQueryFactory queryFactory;
+
+    @Override
+    public List<AssetResponse.CountOutDTO.CountCategory> countByCategory() {
+        QCategory c = QCategory.category;
+        return queryFactory
+                .select(Projections.constructor(AssetResponse.CountOutDTO.CountCategory.class, c.categoryName, c.categoryName.count()))
+                .from(c)
+                .groupBy(c.categoryName)
+                .fetch();
+    }
+
+    @Override
+    public List<AssetResponse.CountOutDTO.CountSubCategory> countBySubCategory() {
+        QCategory c = QCategory.category;
+        return queryFactory
+                .select(Projections.constructor(AssetResponse.CountOutDTO.CountSubCategory.class, c.categoryName, c.subCategoryName, c.subCategoryName.count()))
+                .from(c)
+                .groupBy(c.subCategoryName)
+                .fetch();
+    }
 }
