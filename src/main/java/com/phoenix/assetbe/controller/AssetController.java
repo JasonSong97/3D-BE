592823854950d295faef4bea19d33de800f4d079
BE_YangJinho <real.great.code@gmail.com>
2023-06-07 10:12:40 +0900

    feat : #35 AssetController countByCategory

---
+++ src/main/java/com/phoenix/assetbe/controller/AssetController.java
@@ -0,0 +1,23 @@
+package com.phoenix.assetbe.controller;
+
+import com.phoenix.assetbe.dto.ResponseDTO;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RequiredArgsConstructor
+@RestController
+public class AssetController {
+
+    private final AssetService assetService;
+
+    @GetMapping("/assets/count")
+    public ResponseEntity<?> countByCategory(){
+        AssetResponse.CountOutDTO countOutDTO = assetService.countByCategory();
+        ResponseDTO<?> responseDTO = new ResponseDTO<>(countOutDTO);
+        return ResponseEntity.ok().body(responseDTO);
+    }
+}
