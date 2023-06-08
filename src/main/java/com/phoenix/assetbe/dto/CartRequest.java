bae35d5aef2c2157a5e5fed4fd7ec5f133e76c39
HJoo <yoon3798@naver.com>
2023-06-08 18:07:19 +0900

    feat : #20 장바구니 DTO 수정

--- src/main/java/com/phoenix/assetbe/dto/CartRequest.java
+++ src/main/java/com/phoenix/assetbe/dto/CartRequest.java
@@ -3,6 +3,7 @@ package com.phoenix.assetbe.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotEmpty;
 import java.util.List;
 
 public class CartRequest {
@@ -11,8 +12,21 @@ public class CartRequest {
     @Setter
     public static class AddCartInDTO{
 
+        @NotEmpty(message = "유저 id를 입력해주세요. ")
         private Long userId;
 
+        @NotEmpty(message = "장바구니에 담을 에셋을 입력해주세요. ")
         private List<Long> assets;
+    }
+
+    @Getter
+    @Setter
+    public static class DeleteCartInDTO{
+
+        @NotEmpty(message = "유저 id를 입력해주세요. ")
+        private Long userId;
+
+        @NotEmpty(message = "장바구니에서 삭제할 에셋을 입력해주세요. ")
+        private List<Long> carts;
     }
 }
