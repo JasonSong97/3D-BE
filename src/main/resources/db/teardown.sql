2d3112af34ef8fc0a96a768149198ac33700563a
HJoo <yoon3798@naver.com>
2023-06-10 15:43:36 +0900

    chore : #1 teardown 수정

--- src/main/resources/db/teardown.sql
+++ src/main/resources/db/teardown.sql
@@ -1,16 +1,20 @@
 -- 모든 제약 조건 비활성화
 SET REFERENTIAL_INTEGRITY FALSE;
 truncate table user_tb;
-truncate table wishlist_tb;
-truncate table order_tb;
-truncate table payment_tb;
-truncate table orderproduct_tb;
-truncate table faq_tb;
-truncate table qna_tb;
+truncate table asset_tb;
+truncate table asset_tag_tb;
+truncate table asset_category_tb;
+truncate table asset_sub_category_tb;
+truncate table category_tb;
+truncate table sub_category_tb;
+truncate table tag_tb;
+truncate table my_asset_tb;
+truncate table review_tb;
+truncate table refresh_token_tb;
 truncate table cart_tb;
-truncate table verifiedcode_tb;
-truncate table refreshtoken_tb;
-# truncate table error_log_tb;
-# truncate table login_log_tb;
+truncate table order_tb;
+truncate table order_product_tb;
+truncate table payment_tb;
+truncate table wishlist_tb;
 SET REFERENTIAL_INTEGRITY TRUE;
 -- 모든 제약 조건 활성화
