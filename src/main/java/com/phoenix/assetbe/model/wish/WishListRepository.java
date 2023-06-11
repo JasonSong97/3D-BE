bb09bb0e03dd3cb486a721ad3ed0df748f2b156c
BE_YangJinho <real.great.code@gmail.com>
2023-06-11 13:28:28 +0900

    feat : #16 WishListRepository 작성

--- src/main/java/com/phoenix/assetbe/model/wish/WishListRepository.java
+++ src/main/java/com/phoenix/assetbe/model/wish/WishListRepository.java
@@ -1,6 +1,9 @@
 package com.phoenix.assetbe.model.wish;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface WishListRepository extends JpaRepository<WishList, Long> {
+    @Query("SELECT w.id FROM WishList w WHERE w.asset.id = :assetId AND w.user.id = :userId")
+    Long findIdByAssetIdAndUserId(Long assetId, Long userId);
 }
