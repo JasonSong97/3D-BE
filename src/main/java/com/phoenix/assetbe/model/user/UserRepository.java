222ab754183aed90f8bb00b164530faebcd29431
BE_YangJinho <real.great.code@gmail.com>
2023-06-11 13:27:17 +0900

    feat : #16 UserRepository 작성

--- src/main/java/com/phoenix/assetbe/model/user/UserRepository.java
+++ src/main/java/com/phoenix/assetbe/model/user/UserRepository.java
@@ -13,4 +13,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
 
     boolean existsByEmail(String email);
+
+    @Query("SELECT u.id FROM User u WHERE u.email = :email")
+    Optional<Long> findIdByEmail(String email);
 }
