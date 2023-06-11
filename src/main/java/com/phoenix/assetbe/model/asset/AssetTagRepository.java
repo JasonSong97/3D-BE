7663a020bea9d8f0190fe1136585cd4ecb39e4a0
BE_YangJinho <real.great.code@gmail.com>
2023-06-11 13:24:33 +0900

    feat : #16 AssetTagRepository 작성

--- src/main/java/com/phoenix/assetbe/model/asset/AssetTagRepository.java
+++ src/main/java/com/phoenix/assetbe/model/asset/AssetTagRepository.java
@@ -1,8 +1,12 @@
 package com.phoenix.assetbe.model.asset;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface AssetTagRepository extends JpaRepository<AssetTag, Long> {
-
+    @Query("SELECT DISTINCT at.tag.tagName FROM AssetTag at WHERE at.asset.id = :assetId ORDER BY at.tag.tagName ASC")
+    List<String> findTagNamesByAssetId(Long assetId);
 }
 
