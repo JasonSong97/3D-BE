33536d0f771dab4c60ba6ddf904d1abd1b9be731
Jason_song97 <107292103+JasonSong97@users.noreply.github.com>
2023-06-12 11:48:56 +0900

    test: #3 통합 테스트 환경 세팅

--- src/test/java/com/phoenix/assetbe/controller/UserControllerTest.java
+++ src/test/java/com/phoenix/assetbe/controller/UserControllerTest.java
@@ -1,44 +1,68 @@
 package com.phoenix.assetbe.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.phoenix.assetbe.core.MyRestDoc;
+import com.phoenix.assetbe.core.auth.session.MyUserDetails;
+import com.phoenix.assetbe.core.dummy.DummyEntity;
+import com.phoenix.assetbe.dto.UserInDTO;
 import com.phoenix.assetbe.model.user.User;
 import com.phoenix.assetbe.model.user.UserRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.security.test.context.support.TestExecutionEvent;
+import org.springframework.security.test.context.support.WithUserDetails;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.transaction.annotation.Transactional;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.BDDMockito.then;
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import javax.persistence.EntityManager;
+
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
-@DisplayName("회원 API")
+@ActiveProfiles("test")
 @Sql("classpath:db/teardown.sql")
-@AutoConfigureMockMvc
+@RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
-public class UserControllerTest {
+@AutoConfigureMockMvc
+@Transactional
+public class UserControllerTest extends MyRestDoc {
+
+    private DummyEntity dummy = new DummyEntity();
+
     @Autowired
     private MockMvc mockMvc;
     @Autowired
-    private ObjectMapper om;
-
-    //이메일 전송 테스트
-    @MockBean
-    JavaMailSender javaMailSender;
-
+    private ObjectMapper objectMapper;
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private EntityManager entityManager;
+    @MockBean
+    JavaMailSender javaMailSender; //이메일 전송 테스트
+
+    @BeforeEach
+    public void setUp() {
+
+        userRepository.save(dummy.newUser("유", "현주")); // id 순서 주의
+        userRepository.save(dummy.newUser("송", "재근"));
+        userRepository.save(dummy.newUser("양", "진호"));
+        userRepository.save(dummy.newUser("이", "지훈"));
+
+        entityManager.clear();
+    }
 
 }
